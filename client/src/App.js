a6e723560f240ff75ba1f1f5916ea6937d04b12e
Sebastián Cerutti <93475201+Scerutti@users.noreply.github.com>
2022-04-26 16:32:35 -0300

    make something

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,19 @@ import Detail from "./Components/Detail/Detail.jsx";
 import CreateDog from "./Components/CreateDog/CreateDog.jsx"
 import Error404 from "./Components/Error404/Error404.jsx"
 
+
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
-	  <Switch>
-	  	<Route exact path = "/" component = {LandingPage}/>
-            	<Route  exact path = '/home' component = {Home}/>
-            	<Route exact path = "/dogs/create" component = {CreateDog}/>
-            	<Route path = "/dogs/:id" component = {Detail}/>
-		<Route path ="*" component={Error404}/>
-	  </Switch>
-	</div>   
+			    <Switch>
+            <Route exact path = "/" component = {LandingPage}/>
+            <Route  exact path = '/home' component = {Home}/>
+            <Route exact path = "/dogs/create" component = {CreateDog}/>
+            <Route path = "/dogs/:id" component = {Detail}/>
+            <Route path='*' component={Error404} />
+			  </Switch>
+		  </div>   
     </BrowserRouter>
   );
 }
---
+++ client/src/Components/Error404/Error404.jsx
@@ -0,0 +1,9 @@
+function error (){
+    return (
+    <div>
+        <h1>ERROR 404</h1>
+        <h5>Page not found</h5>
+    </div>
+)}
+
+export default error;
