4a94bd1d9bc5b92e2b6e6c3d3dab1756244f53ae
Sebastián Cerutti <93475201+Scerutti@users.noreply.github.com>
2022-04-26 16:21:04 -0300

    Adding Page Error

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,18 @@ import LandingPage from './Components/LandingPage/LandingPage.jsx';
 import Home from './Components/Home/Home.jsx';
 import Detail from "./Components/Detail/Detail.jsx";
 import CreateDog from "./Components/CreateDog/CreateDog.jsx"
-
+import Error404 from "./Components/Error404/Error404.jsx"
 
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
-			    <Switch>
-            <Route exact path = "/" component = {LandingPage}/>
-            <Route  exact path = '/home' component = {Home}/>
-            <Route exact path = "/dogs/create" component = {CreateDog}/>
-            <Route path = "/dogs/:id" component = {Detail}/>
+	  <Switch>
+	  	<Route exact path = "/" component = {LandingPage}/>
+            	<Route  exact path = '/home' component = {Home}/>
+            	<Route exact path = "/dogs/create" component = {CreateDog}/>
+            	<Route path = "/dogs/:id" component = {Detail}/>
+		<Route path ="*" component={Error404}/>
 			  </Switch>
 		  </div>   
     </BrowserRouter>
